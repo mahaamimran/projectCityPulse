0a79e922ccc6f393f150900afa37f00ec940a7b6
mahaamimran <mahaamimran@gmail.com>
2023-11-22 10:51:40 +0500

    amber

--- lib/home.dart
+++ lib/home.dart
@@ -14,7 +14,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
         // two rectangles in two columns
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.blueGrey[50],
         body: ListView(
           children: [
             SizedBox(height: MediaQuery.of(context).size.height * 0.03),
