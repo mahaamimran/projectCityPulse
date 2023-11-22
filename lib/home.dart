27653e3b1d9311265144ecff02b45d1a4767d705
hashir-ayaz <hashirsmartyy@gmail.com>
2023-11-22 10:12:58 +0500

    pls accept

--- lib/home.dart
+++ lib/home.dart
@@ -13,32 +13,30 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // two rectangles in two columns
-      backgroundColor: Colors.white,
-      body: ListView(
-        children: [  
-          SizedBox(height: MediaQuery.of(context).size.height * 0.03),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // border radius
+        // two rectangles in two columns
+        backgroundColor: Colors.blue,
+        body: ListView(
+          children: [
+            SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // border radius
 
-              Container(
-                height: MediaQuery.of(context).size.height * 0.3,
-                width: MediaQuery.of(context).size.width * 0.4,
-                color:const Color.fromRGBO(255, 239, 244, 1),
-
-
-              ),
-              SizedBox(width: MediaQuery.of(context).size.width * 0.05),
-              Container(
-                height: MediaQuery.of(context).size.height * 0.3,
-                width: MediaQuery.of(context).size.width * 0.4,
-                color:  const Color.fromRGBO(145, 60, 240, 1),
-              ),
-            ],
-          ),
-      ],)
-    );
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.3,
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  color: const Color.fromRGBO(255, 239, 244, 1),
+                ),
+                SizedBox(width: MediaQuery.of(context).size.width * 0.05),
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.3,
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  color: const Color.fromRGBO(145, 60, 240, 1),
+                ),
+              ],
+            ),
+          ],
+        ));
   }
 }
