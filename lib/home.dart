c6d6b6d1a48f720aa2b605ff96b6bdc8cd08c477
mahaamimran <mahaamimran@gmail.com>
2023-11-23 17:50:27 +0500

    home page boxes structure

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,3 @@
-//import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -14,29 +12,88 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
         // two rectangles in two columns
-        backgroundColor: Colors.black,
+        // backgroundColor: Colors.amber,
         body: ListView(
+      children: [
+        // space beneath navigation bar
+        SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            SizedBox(height: MediaQuery.of(context).size.height * 0.03),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+            // border radius
+            // first column
+            Column(
               children: [
-                // border radius
-
+                // account information
                 Container(
                   height: MediaQuery.of(context).size.height * 0.3,
                   width: MediaQuery.of(context).size.width * 0.4,
-                  color: const Color.fromRGBO(255, 239, 244, 1),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 241, 225, 230),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
                 ),
-                SizedBox(width: MediaQuery.of(context).size.width * 0.05),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.025),
+                // vehicles
                 Container(
-                  height: MediaQuery.of(context).size.height * 0.3,
+                  height: MediaQuery.of(context).size.height * 0.15,
                   width: MediaQuery.of(context).size.width * 0.4,
-                  color: const Color.fromRGBO(145, 60, 240, 1),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 28, 138, 131),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.025),
+                // predictions
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.18,
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 176, 218, 213),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
                 ),
               ],
             ),
+            SizedBox(width: MediaQuery.of(context).size.width * 0.05),
+            // second column
+            Column(
+              children: [
+                // parking
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.25,
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  decoration: BoxDecoration(
+                    color: const Color.fromRGBO(145, 60, 240, 1),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.025),
+                // fine payments
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.25,
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 214, 195, 235),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.025),
+                // traffic updates
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.13,
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 66, 46, 87),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              
+              ],
+            ),
           ],
-        ));
+        ),
+      ],
+    ));
   }
 }
